c99591c772505191602cd120142f7cef1b9c74f5
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-04 13:59:59 -0700

    print lower alpha except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(ord('a'), ord('z')):
+for alpha in range(ord('a'), ord('z') + 1):
     if alpha != ord('e') or alpha != ord('q'):
         print("{}".format(chr(ord(alpha))), end='')
