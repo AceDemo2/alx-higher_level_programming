0800d29941fde842886f961ca2d72cd17eb43639
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-04 04:14:18 -0700

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - check for loop in LL
+ * @list: head of linked list
+ *
+ * Description - check for loops in LL
+ * Return: 1 if cycled, 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *i = list, *j = list;
+	if (!list)
+		return (0);
+	while (j && j->next)
+	{
+		j = j->next;
+		i = i->next->next;
+		if (i == j)
+			return (1);
+	}
+	return (0);
+}
