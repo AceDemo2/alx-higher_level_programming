d39970007dcd823f754fc35990e0dc403e17d997
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-05 02:39:39 -0700

    to upper cas

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        l = -(-number % 10)
-    else:
-        l = number % 10
-    print(l)
+    l = number % 10
+    print(l, end='')
     return(l)
