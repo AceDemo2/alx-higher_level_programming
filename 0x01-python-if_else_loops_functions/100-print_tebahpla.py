6abb7b086fc7df1b060e70281000f81ceef70618
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-05 05:04:38 -0700

    alpha rev alternating

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for alpha in range(ord('a'), ord('z') + 1, -1):
     if ord('A') <= ord(alpha) >= ord('Z'):
         alpha = chr((ord(alpha) - 32))
-    print(f'{}', alpha, end='')
+    print('{}'.format(alpha), end='')
