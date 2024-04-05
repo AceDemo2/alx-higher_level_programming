32a3a4e49ab7fb49ea25ff77fdc187f98af0090c
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-05 01:36:56 -0700

    0-99 different digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num in range(10):
     for num2 in range(10):
         if num == 8 and num2 == 9:
-            print("{}{}".format(num, num2), end='')
+            print("{}{}".format(num, num2))
         elif num2 > num and num != num2:
-            print("{}{}, ".format(num, num2))
+            print("{}{}, ".format(num, num2, end=''))
