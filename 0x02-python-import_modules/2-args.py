5aa6989968e718127b23d3d093e65020516ab03b
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-06 03:57:13 -0700

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    le = len(sys.argv)
+    if le <= 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(le))
+        while i < le:
+            print("{} {}".format(i, argv[i]))
+            i += 1
