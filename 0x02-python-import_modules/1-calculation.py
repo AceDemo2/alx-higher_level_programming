7c738bcb2065b4f351c415bf21d80004dbc1cbdc
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-06 03:16:54 -0700

    cal

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     import calculator_1
     a = 10
     b = 5
-    calculator_1.add(a, b)
-    calculator_1.sub(a, b)
-    calculator_1.mul(a, b)
-    calculator_1.div(a, b)
+    add = calculator_1.add(a, b)
+    sub = calculator_1.sub(a, b)
+    mul = calculator_1.mul(a, b)
+    div = calculator_1.div(a, b)
+    s = [add, sub, mul, div]
+    for c in s:
+        print("{} + {} = {}".format(a, b, c))
