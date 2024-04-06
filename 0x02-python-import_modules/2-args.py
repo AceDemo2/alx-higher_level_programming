4fb50c8efd5a9554808195824cc8fb3d00ecbcc6
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-06 04:26:06 -0700

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     le = len(sys.argv)
     pl = "s" if le >= 3 else ""
-    si = ":" if le >= 2 else "."
+    si = ":" if le >= 2 or le == 0 else "."
     print("{} argument{}{}".format((le - 1 if le > 0 else 0), pl, si))
     i = 1
     while i < le:
