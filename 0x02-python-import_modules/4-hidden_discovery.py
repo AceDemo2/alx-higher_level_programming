96ecb42320d9894c9c4fd8e15956696bd788c3e2
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-06 14:18:51 -0700

    print name exept __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     na = dir(hidden_4)
-    for i in na
-    if i[:2] != "__":
-        print(i)
+    for i in na:
+        if i[:2] != "__":
+            print(i)
