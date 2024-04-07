cf96a18ee16baa490d39e72b7b4df41549c0c833
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-07 09:48:29 -0700

    find element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    for i in my_list:
-        if i == my_list[idx]:
-            return (i)
+    return (my_list[idx])
