c8aa62695de5bf0224732f6740de41f3acd11926
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-07 13:21:57 -0700

    find and replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     s = my_list
-    s[idx] = element
+    if 0 <= idx < len(s):
+        s[idx] = element
     for i in s:
         print("{}".format(i))
