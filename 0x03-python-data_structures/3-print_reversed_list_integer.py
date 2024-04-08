41f23b291eaa1dc424ca5c1235bd6f91fae8f3fc
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-08 04:47:42 -0700

    find and replace

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    s = my_list[::-1]
-    for i in s:
-        print("{:d}".format(i))
+    if my_list != None:
+        s = my_list[::-1]
+        for i in s:
+            print("{:d}".format(i))
