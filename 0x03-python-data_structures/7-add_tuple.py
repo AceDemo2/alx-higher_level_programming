81e735f8ff3e08dfac1b09a719a9075dde9b4f8d
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-08 17:39:22 -0700

    print matrix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    t1 = a + (0, 0)
-    t2 = b + (0, 0)
+    t1 = tuple_a + (0, 0)
+    t2 = tuple_b + (0, 0)
     return (t1[0] + t2[1], t1[0] + t2[1])
