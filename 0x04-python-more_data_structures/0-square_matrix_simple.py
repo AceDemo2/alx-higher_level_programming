a500f6e63e067559284b44aa95671e6ee4c3904f
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-09 23:57:10 -0700

    square of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         k = list(map(lambda x: x**2, matrix[i]))
-        print(k, end=', ' if i != len(matrix) - 1 else '')
+        print(list[k], end=', ' if i != len(matrix) - 1 else '')
