c78d30405e70cb6f72e2905d4bb1504548f830c2
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-12 02:02:02 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    n = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            my_list[i] = replace
+            n.append(replace)
+        else:
+            n.append(my_list[i])
+
