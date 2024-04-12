09f3abb2b0118018276b3fb0527f0e733d263c69
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-12 11:32:09 -0700

    update dic

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary.key:
+    for i in a_dictionary.key():
         if i == key:
             a_dictionary[i] = value
     return a_dictionary
