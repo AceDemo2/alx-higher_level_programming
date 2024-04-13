00fa358d25a6861742bea9f7ff5467c03f3f5447
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-13 03:48:01 -0700

    max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3i
 def best_score(a_dictionary):
-    l = list(a_dictionary)
-    i = a_dictionary[l[0]]
-    k = ""
+        k = ""
     if a_dictionary:
+        l = list(a_dictionary)
+        i = a_dictionary[l[0]]
         for j in a_dictionary:
             if a_dictionary[j] > i:
                 i = a_dictionary[j]
