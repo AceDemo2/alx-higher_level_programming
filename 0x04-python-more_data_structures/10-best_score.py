53bb3179412f4174b0f4aaa4f0aedf24bc17922d
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-13 04:02:52 -0700

    max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,10 @@ def best_score(a_dictionary):
     if a_dictionary:
         l = list(a_dictionary)
         k = l[0]
-        i = a_dictionary[k]
+        value = a_dictionary[k]
         for j in a_dictionary:
-            if a_dictionary[j] > i:
-                i = a_dictionary[j]
+            if a_dictionary[j] > value:
+                value = a_dictionary[j]
                 k = j
         return k
     return None
