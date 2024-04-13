e20acfe83aceb976ce316045cb3d31dce3221cab
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-13 14:11:46 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,14 @@
 def roman_to_int(roman_string):
     sym = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    i = 0
     p = 0
-    for s, v in sym.items():
-        if s == roman_string[i]:
-            if v < p:
-                total -= v
-            else:
-                total += v
-            p = v
-            i += 1
-        else:
+    for i in roman_string:
+        v = sym[i]
+        if v = None:
             return 0
+        if v < p:
+            total -= v
+        else:
+            total += v
+            p = v
     return total
-
