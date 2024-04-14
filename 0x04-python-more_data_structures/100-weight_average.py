6637bf3954c67e8efa2c89ee3d7d598587d4efc6
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-13 17:02:09 -0700

    average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,10 @@
 def weight_average(my_list=[]):
     num = 0
     denum = 0
-    for i in my_list:
-        num += i[0] * i[1]
-        denum += i[1]
-    return num / denum
+    tot = 0
+    if my_list:
+        for i in my_list:
+            num += i[0] * i[1]
+            denum += i[1]
+        tot = num / denum
+    return tot
