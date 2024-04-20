0151b24b877293b1abcf44ac61edc8f61439f9bd
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-20 10:20:46 -0700

    sfe func

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as i:
-        write("Exception: " + str(i) + "\n")
+        stderr.write("Exception: " + str(i) + "\n")
         return None
