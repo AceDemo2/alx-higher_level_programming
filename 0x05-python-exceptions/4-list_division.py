b1e599ff509a04a1a0d0815dd967363a0aa1382c
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-20 09:41:28 -0700

    div list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             n = 0
         finally:
-            li += n
+            li.append(n)
     return li
