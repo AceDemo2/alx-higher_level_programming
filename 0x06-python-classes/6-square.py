9918a7cf6c75ff6009a8670b322d792f1dedfb1d
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-22 09:31:18 -0700

    print spacrs accordingly

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,8 +12,8 @@ class Square:
         """initialize square
         Args: size (int): size of the square and position
         """
-       self.size = size
-       self.position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
