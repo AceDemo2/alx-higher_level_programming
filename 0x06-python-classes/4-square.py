8d204e20180f27bb6e0dd529483a5e7d9d01094b
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-21 20:02:59 -0700

    getter and sette

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,7 +15,7 @@ class Square:
     @property
     def size(self):
         self.__size = size
-    @size.setter
+    @s.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError('size must be an integer')
