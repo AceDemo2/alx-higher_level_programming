8a1da4a0d7cdc629bfc106c16ee8a3ab72cb579d
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-21 20:11:30 -0700

    getter and sette

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
         """initialize square
         Args: size (int): size of the square
         """
-    
+        self.__size = size 
     @property
     def size(self):
         return self.__size
