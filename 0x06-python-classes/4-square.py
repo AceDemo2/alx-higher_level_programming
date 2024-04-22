b51627824c508a5a6af022aabf37704b4d45b851
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-21 19:59:17 -0700

    public instance

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+class Square
+"""
+
+
+class Square:
+    """
+    define square
+    """
+    def __init__(self, size=0):
+        """initialize square
+        Args: size (int): size of the square
+        """
+    @property
+    def size(self):
+        self.__size = size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
