bba151b1674af13b6d589bef07cf30a2dc3146cc
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-21 20:04:08 -0700

    getter and sette

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
         """
     @property
     def size(self):
-        self.__size = size
+       return self.__size
     @s.setter
     def size(self, value):
         if type(value) is not int:
