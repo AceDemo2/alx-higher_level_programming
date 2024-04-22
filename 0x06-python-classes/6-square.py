d5542748159822ab088fceb779e05704a372984d
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-22 06:13:58 -0700

    print spacrs accordingly

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,7 +14,7 @@ class Square:
         """
         self.__size = size
         self.__position = position
-    
+
     @property
     def position(self):
         return self.__position
@@ -22,9 +22,9 @@ class Square:
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) != 2 or value[0] < 0 \
-        or value[1] < 0 or type(value[0]) is not int \
-        or type(value[1]) is not int:
-            raise TypeError('position must be a tuple of 2 positive integers')
+            or value[1] < 0 or type(value[0]) is not int \
+                or type(value[1]) is not int:
+                    raise TypeError('position must be a tuple of 2 positive integers')
 
     @property
     def size(self):
