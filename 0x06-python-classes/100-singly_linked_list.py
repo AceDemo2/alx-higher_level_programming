75b2fd9f4a7f0207f7e4c82bd394c8e4bbcefb4b
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-23 07:16:34 -0700

    sqr

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""create class"""
+class Node:
+    """define Node"""
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError('data must be an integer')
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if type(value) is not Node or value is not None:
+            raise TypeError('next_node must be a Node object')
+        else:
+            self.__next_node = value
+
+"""create SinglyLinkedList class"""
+class SinglyLinkedList:
+    """define SinglyLinkedList"""
+    def __init__(self):
+        self.head = None
+    def sorted_insert(self, value):
+        new = Node(value)
+        if self.head is None or self.head.data >= value:
+            new.next_node = self.head
+            self.head = new
+        else:
+            i = self.head
+            while i and i.next_node.data < value:
+                i = i.next_node
+            new.next_node = i.next_node
+            i.next_node = new
+    
+    def __str__(self):
+        j = ""
+        i = self.head
+        while i:
+            j += str(i.data) + "\n"
+            i = i.next_node
+        return j
