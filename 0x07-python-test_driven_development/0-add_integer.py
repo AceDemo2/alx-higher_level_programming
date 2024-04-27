64f1f4e731f4a0a12f5426baaf7c8a1fccc68d69
AceDemo2 <142119014+AceDemo2@users.noreply.github.com>
2024-04-27 06:37:47 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,30 @@
-t case: Adding positive numbers
-2, 3, 5
+#!/usr/bin/python3
+"""
 
-# Test case: Adding negative numbers
--2, -3, -5
+This module is composed by a function that adds two numbers
 
-# Test case: Adding zero
-0, 0, 0
+"""
 
-# Test case: Adding maximum integer value
-2147483647, 1, 2147483648
 
-# Test case: Testing for potential overflow
-2147483647, 10, OverflowError
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        The addition of the two given numbers
+
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
