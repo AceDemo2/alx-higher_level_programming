1fb64cc188c639383a4d3992a6e7530ec568851d
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-29 16:52:12 -0700

    read

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-// read and print 
+// read and print
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-    return;
   }
   else
-    console.log(data)
+    console.log(data);
 });
