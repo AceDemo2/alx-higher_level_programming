19fae38e010f9886b043cb3c6893e21b9b410120
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-29 16:41:58 -0700

    reand and print

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,13 @@
-#!/bin/bash
-const = require('fs')
-fs.readFile([1], 'utf-8', (err, data) => {
-	if (err)
-		console.log(err)
+#!/usr/bin/node
+// read and print 
+
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
 	else
 		console.log(data)
 });
