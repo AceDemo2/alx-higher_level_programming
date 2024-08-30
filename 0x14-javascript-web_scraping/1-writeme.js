b2f0211f35ff2f8a48f71c429b6f98d0657adabd
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-29 17:13:21 -0700

    write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-// read and print 
-
+// write 
 
 const fs = require('fs');
-fs.writeFile(process.argv[3], 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
