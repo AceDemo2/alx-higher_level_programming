fc9dbe9d70cd339aebe1845fe0eb3634846a7d48
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-30 18:43:34 -0700

    get name

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// print
+
+const rq = require('request');
+const api = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+rq(api, (err, resp, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const data = JSON.parse(body).characters;
+  for (let i = 0; i < data.length; i++) {
+    rq(data[i], (err, resp, body) => {
+      if (err) {
+        console.log(err);
+	return;
+      }
+      const name = JSON.parse(body).name;
+      console.log(name);
+    });
+  }
+});
